8160a3b15383ad46544a1d22215df8e341bfb42e
Lalit Kanta Dibyadarshan <lkd.darshan24@gmail.com>
2021-05-29 04:07:22 +0530

    [foundation] added rollup bundler

---
+++ rollup.config.ts
@@ -0,0 +1,42 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+import visualizer from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+const production = process.argv.includes('prod');
+
+export default {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+		},
+		{
+			file: pkg.module,
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	external: ['react', 'react-dom'],
+	plugins: [
+		resolve(),
+		commonjs(),
+		postcss({
+			extract: true,
+			modules: true,
+			use: ['sass'],
+		}),
+		typescript({
+			useTsconfigDeclarationDir: true,
+		}),
+		terser(),
+		visualizer({
+			filename: 'bundle-analysis.html',
+			open: !production,
+		})
+	],
+};
